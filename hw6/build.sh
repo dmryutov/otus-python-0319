15bd02de9c7c9f9e93608e3a0094db45e6f19180
DRyutov <DRyutov@optimum.omd.ru>
2019-04-18 12:51:33 +0300

    Fix PostgreSQL for HW 6

--- hw6/build.sh
+++ hw6/build.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-# Project settings
+echo "=== Load settings (1/8) ==="
 PROJECT_NAME=hasker
 PROJECT_PATH=$(pwd)
 SECRET_KEY="$(date +%s | sha256sum | base64 | head -c 50)"
 CONFIG="main.settings.production"
-
 DB_NAME=hasker
 DB_USER=hasker
 DB_PASSWORD=Hasker12345
 
 
-echo "=== Install packages (1/7) ==="
+echo "=== Install packages (2/8) ==="
 yum clean all
 yum install -y epel-release
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
@@ -19,21 +18,23 @@ yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x
 yum install -y sudo wget git gcc-c++ make python36u python36u-devel python36u-pip nginx postgresql96-server postgresql96-contrib postgresql96-devel
 
 
-echo "=== Install hack for systemd (2/7) ==="
+echo "=== Install hack for systemd (3/8) ==="
 wget https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl.py -O /usr/local/bin/systemctl
 chmod +x /usr/local/bin/systemctl
 
 
-echo "=== Configure PostgreSQL (3/7) ==="
+echo "=== Configure PostgreSQL (4/8) ==="
 export PATH="$PATH:/usr/pgsql-9.6/bin/"
 postgresql96-setup initdb
 echo "listen_addresses='*'" >> /var/lib/pgsql/9.6/data/postgresql.conf
+sed -ie 's/ident/trust/g' /var/lib/pgsql/9.6/data/pg_hba.conf
+sed -ie 's/peer/trust/g' /var/lib/pgsql/9.6/data/pg_hba.conf
 sudo -u postgres /usr/pgsql-9.6/bin/pg_ctl start -D /var/lib/pgsql/9.6/data -s -o "-p 5432" -w -t 300
 su postgres -c "psql -c \"CREATE USER ${DB_USER} PASSWORD '${DB_PASSWORD}'\""
 su postgres -c "psql -c \"CREATE DATABASE ${DB_NAME} OWNER ${DB_USER}\""
 
 
-echo "=== Configure Nginx (4/7) ==="
+echo "=== Configure Nginx (5/8) ==="
 cat > /etc/nginx/conf.d/${PROJECT_NAME}.conf << EOF
 server {
     listen 80;
@@ -59,7 +60,7 @@ EOF
 /usr/local/bin/systemctl status nginx
 
 
-echo "=== Configure uWSGI (5/7) ==="
+echo "=== Configure uWSGI (6/8) ==="
 mkdir -p /run/uwsgi
 mkdir -p /usr/local/etc
 cat > /usr/local/etc/${PROJECT_NAME}-uwsgi.ini << EOF
@@ -81,7 +82,7 @@ env=DB_PASSWORD=${DB_PASSWORD}
 EOF
 
 
-echo "=== Configure Django (6/7) ==="
+echo "=== Configure Django (7/8) ==="
 pip3.6 install -r requirements.txt
 for CMD in "makemigrations" "migrate" "collectstatic --link --noinput"
 do
@@ -94,5 +95,5 @@ do
 done
 
 
-echo "=== Start Django (7/7) ==="
+echo "=== Start Django (8/8) ==="
 uwsgi --ini /usr/local/etc/${PROJECT_NAME}-uwsgi.ini &
